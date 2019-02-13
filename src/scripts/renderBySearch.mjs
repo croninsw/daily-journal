f5d73d320f693701b68856f8fe09104df1af5deb
Sam Cronin <croninsw@gmail.com>
2019-02-13 11:21:44 -0600

    Debugging filter function

--- src/scripts/renderBySearch.mjs
+++ src/scripts/renderBySearch.mjs
@@ -2,18 +2,35 @@ import API from "./data.mjs"
 import makeJournalEntryComponent from "./entryComponent.mjs"
 import addEntriestoDOM from "./entriesDOM.mjs"
 
-const renderBySearch = input => {
+// const renderBySearch = input => {
+//     API.getJournalEntries()
+//         .then(entryArray => {
+//             // entry is not defined because...it's not passed in somewhere?
+//             for (entry of entryArray) {
+//                 if (Object.values(entry).inlcudes(input)) {
+//                     const html = makeJournalEntryComponent(entry)
+//                     addEntriestoDOM(html)
+
+//                 }
+//             }
+//         })
+//     }
+
+const renderBySearch = (input) => {
     API.getJournalEntries()
         .then(entryArray => {
-            console.table(entryArray)
-            for (entry of entryArray) {
-                if (Object.values(entry).inlcudes(input)) {
-                    const html = makeJournalEntryComponent(entry)
-                    addEntriestoDOM(html)
+            console.log(entryArray[0])
+            let filterInput = entryArray.filter(entry => {
+                (Object.values(entry).includes(input))
+            } )
+            filterInput.forEach(entry => {
+                const html = makeJournalEntryComponent(entry)
+                addEntriestoDOM(html)
 
-                }
             }
+            )
         })
-    }
+}
 
     export default renderBySearch
+
